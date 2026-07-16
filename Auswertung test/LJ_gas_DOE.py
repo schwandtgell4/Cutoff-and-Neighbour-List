a3c5b3d07b5dc6bfc3615fcdedcaa8e07d40c164
salimonus <salimfazzani125@gmail.com>
2026-07-16 16:00:59 +0200

    doe run datei

---
+++ Auswertung test/LJ_gas_DOE.py
@@ -0,0 +1,170 @@
+import os
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.constants import R
+from itertools import product
+
+from LJ_gas import ParticleSystem
+from LJ_gas import SimulationParameters
+from LJ_gas import initialize_positions
+from LJ_gas import initialize_velocities
+from LJ_gas import update_neighbour_list
+from LJ_gas import calculate_force
+from LJ_gas import simulate_NVT_step
+from LJ_gas import kinetic_energy
+from LJ_gas import density
+
+n_particles = 2000
+mass_argon = 39.95
+sigma_argon = 0.34
+epsilon_argon = 120 * R * 1e-3
+
+dt = 0.1
+n_steps = 1000
+temperature = 300
+box_length_start = 100
+tau_thermostat = 1
+rij_min = 1e-2
+
+n_update_werte = list(range(1, 11))
+cutoff_faktoren = [1, 2, 3, 5, 10, 20]
+dichte_faktoren = [0.5, 1.0, 1.5]
+seeds = [1, 2, 3, 4, 5]
+
+ordner = "doe_ergebnisse"
+os.makedirs(ordner, exist_ok=True)
+
+ergebnisse = []
+
+kombinationen = list(product(n_update_werte, cutoff_faktoren, dichte_faktoren, seeds))
+
+zufall_reihenfolge = np.random.default_rng(12345)
+zufall_reihenfolge.shuffle(kombinationen)
+
+anzahl = len(kombinationen)
+
+for nummer, kombination in enumerate(kombinationen):
+    n_update = kombination[0]
+    cutoff_faktor = kombination[1]
+    dichte_faktor = kombination[2]
+    seed = kombination[3]
+
+    np.random.seed(seed)
+
+    box_length = box_length_start / dichte_faktor ** (1 / 3)
+    r_cut = cutoff_faktor * sigma_argon
+
+    sim = SimulationParameters(
+        dt=dt,
+        n_steps=n_steps,
+        temperature=temperature,
+        box_length=box_length,
+        tau_thermostat=tau_thermostat,
+        rij_min=rij_min,
+        r_cut=r_cut,
+        use_cutoff=True
+    )
+
+    ps = ParticleSystem(n_particles)
+    ps.mass[:] = mass_argon
+    ps.sigma[:] = sigma_argon
+    ps.epsilon[:] = epsilon_argon
+
+    initialize_positions(ps, sim.box_length)
+    initialize_velocities(ps, sim.temperature)
+    update_neighbour_list(ps, sim, 0, n_update)
+    calculate_force(ps, sim)
+
+    temperaturen = []
+    kinetische_energien = []
+
+    e_kin = kinetic_energy(ps)
+    temp = 2 * e_kin * 1000 / (3 * ps.n * R)
+    kinetische_energien.append(e_kin)
+    temperaturen.append(temp)
+
+    start = time.perf_counter()
+
+    for schritt in range(n_steps):
+        simulate_NVT_step(ps, sim, schritt + 1, n_update)
+        e_kin = kinetic_energy(ps)
+        temp = 2 * e_kin * 1000 / (3 * ps.n * R)
+        kinetische_energien.append(e_kin)
+        temperaturen.append(temp)
+
+    dauer = time.perf_counter() - start
+
+    temperaturen = np.array(temperaturen)
+    kinetische_energien = np.array(kinetische_energien)
+
+    temperatur_abweichung = np.mean(np.abs(temperaturen - np.mean(temperaturen)))
+    energie_abweichung = np.mean(np.abs(kinetische_energien - np.mean(kinetische_energien)))
+    geschwindigkeit = n_steps / dauer
+    aktuelle_dichte = density(ps, sim)
+
+    ergebnisse.append({
+        "n_update": n_update,
+        "cutoff_faktor": cutoff_faktor,
+        "cutoff_radius_nm": r_cut,
+        "dichte_faktor": dichte_faktor,
+        "dichte_g_cm3": aktuelle_dichte,
+        "seed": seed,
+        "temperatur_abweichung_K": temperatur_abweichung,
+        "simulationsgeschwindigkeit_schritte_pro_s": geschwindigkeit,
+        "kinetische_energie_abweichung_kJ_mol": energie_abweichung
+    })
+
+    tabelle = pd.DataFrame(ergebnisse)
+    tabelle.to_csv(os.path.join(ordner, "doe_ergebnisse.csv"), index=False)
+
+    print(
+    str(nummer + 1) + "/" + str(anzahl)
+    + ", Laufzeit=" + format(dauer, ".2f") + " s"
+    + ", n=" + str(n_update)
+    + ", rcutoff=" + format(r_cut, ".3f") + " nm"
+    + ", Dichte=" + format(aktuelle_dichte, ".6e") + " g/cm3"
+    + ", Abw. T=" + format(temperatur_abweichung, ".4f") + " K"
+    + ", Abw. Ekin=" + format(energie_abweichung, ".4f") + " kJ/mol",
+    flush=True
+)
+
+
+def heatmap(tabelle, x_name, y_name, z_name):
+    daten = tabelle.pivot_table(index=y_name, columns=x_name, values=z_name, aggfunc="mean")
+
+    plt.figure(figsize=(10, 7))
+    bild = plt.imshow(daten.values, aspect="auto", origin="lower")
+    plt.colorbar(bild, label=z_name)
+    plt.xticks(range(len(daten.columns)), [str(x) for x in daten.columns], rotation=45)
+    plt.yticks(range(len(daten.index)), [str(y) for y in daten.index])
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.title(z_name + " für " + x_name + " und " + y_name)
+
+    for y in range(len(daten.index)):
+        for x in range(len(daten.columns)):
+            plt.text(x, y, format(daten.iloc[y, x], ".3g"), ha="center", va="center")
+
+    plt.tight_layout()
+    dateiname = z_name + "__" + x_name + "__" + y_name + ".png"
+    plt.savefig(os.path.join(ordner, dateiname), dpi=300)
+    plt.close()
+
+
+tabelle = pd.DataFrame(ergebnisse)
+
+ziele = [
+    "temperatur_abweichung_K",
+    "simulationsgeschwindigkeit_schritte_pro_s",
+    "kinetische_energie_abweichung_kJ_mol"
+]
+
+for ziel in ziele:
+    heatmap(tabelle, "n_update", "cutoff_faktor", ziel)
+    heatmap(tabelle, "n_update", "dichte_faktor", ziel)
+    heatmap(tabelle, "cutoff_faktor", "dichte_faktor", ziel)
+
+mittelwerte = tabelle.groupby(["n_update", "cutoff_faktor", "dichte_faktor"], as_index=False).mean(numeric_only=True)
+mittelwerte.to_csv(os.path.join(ordner, "doe_mittelwerte.csv"), index=False)
